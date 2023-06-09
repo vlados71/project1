110d31f6683a67dde38c29c3bce4808068bfac1b
vlados71 <136016810+vlados71@users.noreply.github.com>
2023-06-09 14:16:50 +0300

    Update labirint_0_1667392830.py

--- 824364 - Copy/labirint_0_1667392830.py
+++ 824364 - Copy/labirint_0_1667392830.py
@@ -1,6 +1,5 @@
 from pygame import *
 
-#клас-батько для інших спрайтів
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, size_x, size_y):
         sprite.Sprite.__init__(self)
@@ -14,41 +13,32 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
-#клас головного гравця
 class Player(GameSprite):
-    #метод, у якому реалізовано управління спрайтом за кнопками стрілочкам клавіатури
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_x_speed,player_y_speed):
-        # Викликаємо конструктор класу (Sprite):
         GameSprite.__init__(self, player_image, player_x, player_y, size_x, size_y)
 
         self.x_speed = player_x_speed
         self.y_speed = player_y_speed
-    ''' переміщає персонажа, застосовуючи поточну горизонтальну та вертикальну швидкість'''
     def update(self):  
-        # Спершу рух по горизонталі
         if packman.rect.x <= win_width-80 and packman.x_speed > 0 or packman.rect.x >= 0 and packman.x_speed < 0:
             self.rect.x += self.x_speed
-        # якщо зайшли за стінку, то встанемо впритул до стіни
         platforms_touched = sprite.spritecollide(self, barriers, False)
-        if self.x_speed > 0: # йдемо праворуч, правий край персонажа - впритул до лівого краю стіни
+        if self.x_speed > 0: 
             for p in platforms_touched:
-                self.rect.right = min(self.rect.right, p.rect.left) # якщо торкнулися відразу кількох, то правий край - мінімальний із можливих
-        elif self.x_speed < 0: # йдемо ліворуч, ставимо лівий край персонажа впритул до правого краю стіни
+                self.rect.right = min(self.rect.right, p.rect.left)
+        elif self.x_speed < 0: 
             for p in platforms_touched:
-                self.rect.left = max(self.rect.left, p.rect.right) # якщо торкнулися кількох стін, то лівий край - максимальний
+                self.rect.left = max(self.rect.left, p.rect.right) 
         if packman.rect.y <= win_height-80 and packman.y_speed > 0 or packman.rect.y >= 0 and packman.y_speed < 0:
             self.rect.y += self.y_speed
-        # якщо зайшли за стінку, то встанемо впритул до стіни
         platforms_touched = sprite.spritecollide(self, barriers, False)
-        if self.y_speed > 0: # йдемо вниз
+        if self.y_speed > 0: 
             for p in platforms_touched:
-                # Перевіряємо, яка з платформ знизу найвища, вирівнюємося по ній, запам'ятовуємо її як свою опору:
                 if p.rect.top < self.rect.bottom:
                     self.rect.bottom = p.rect.top
-        elif self.y_speed < 0: # йдемо вгору
+        elif self.y_speed < 0:
             for p in platforms_touched:
-                self.rect.top = max(self.rect.top, p.rect.bottom) # вирівнюємо верхній край по нижніх краях стінок, на які наїхали
-    # метод "постріл" (використовуємо місце гравця, щоб створити там кулю)
+                self.rect.top = max(self.rect.top, p.rect.bottom) 
     def fire(self):
         bullet = Bullet('bulllet1.png', self.rect.centerx, self.rect.top, 30, 35, 10)
         bullets.add(bullet)
@@ -90,7 +80,6 @@ class Enemy_y(GameSprite):
 
 class Bullet(GameSprite):
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
-        # Викликаємо конструктор класу (Sprite):
         GameSprite.__init__(self, player_image, player_x, player_y, size_x, size_y)
         self.speed = player_speed
     def update(self):
@@ -102,25 +91,21 @@ font.init()
 font1 = font.SysFont('arial', 25)
 
 coins_amount=0
-#Створюємо віконце
+
 win_width = 1200
 win_height = 800
 display.set_caption("Лабіринт")
 window = display.set_mode((win_width, win_height))
-back = transform.scale(image.load("background3.jpg"), (win_width, win_height)) 
+back = transform.scale(image.load("background111.jpg"), (win_width, win_height)) 
 
-#Створюємо групу для стін
 barriers = sprite.Group()
 
-#створюємо групу для куль
 bullets = sprite.Group()
 
-#Створюємо групу для монстрів
 monsters = sprite.Group()
 
 coins = sprite.Group()
 
-#Створюємо стіни картинки
 w1 = GameSprite("wall1.png",234,335, 100, 469 )
 w2 = GameSprite("wall2.jpg", 102, 650, 135, 60)
 w3 = GameSprite("wall2.jpg", 0, 500, 135, 60)
@@ -135,7 +120,7 @@ w11 = GameSprite("wall2.jpg", 865, 453, 145, 50)
 w12 = GameSprite("wall1.png", 960, 337, 50, 120)
 w13 = GameSprite("wall2.jpg", 1007, 337, 105, 40)
 w14 = GameSprite("wall2.jpg", 1000, 600, 200, 30)
-#додаємо стіни до групи
+
 barriers.add(w1)
 barriers.add(w2)
 barriers.add(w3)
@@ -150,32 +135,39 @@ barriers.add(w11)
 barriers.add(w12)
 barriers.add(w13)
 barriers.add(w14)
-#створюємо спрайти
+
 coin1 = GameSprite("coin.png", 259, 286, 50, 50)
 coin2 = GameSprite("coin.png", 807, 404, 50, 50)
 coin3 = GameSprite("coin.png", 370, 690, 50, 50)
 coin4 = GameSprite("coin.png", 1112, 75, 50, 50)
+coin5 = GameSprite("coin.png", 1100, 552, 50, 50)
 
 coins.add(coin1)
 coins.add(coin2)
 coins.add(coin3)
 coins.add(coin4)
-
+coins.add(coin5)
 
 packman = Player('packman1.png', 5, win_height - 80, 80, 80, 0, 0)
-monster1 = Enemy_x('enemy22.png', win_width - 800, 37, 85, 85, 200, 100, 1)
-monster2 = Enemy_y('enemy22.png', win_width - 80, 90, 80, 80, 100, 200, 1)
+monster1 = Enemy_x('enemy22.png', win_width - 100, 33, 85, 85, 700, 1100, 1)
+monster2 = Enemy_y('enemy22.png', win_width - 1020, 270, 80, 80, 140, 270, 1)
+monster3 = Enemy_x("enemy22.png", win_width - 825, 630, 100, 100, 330, 710, 1)
+monster4 = Enemy_y("enemy22.png", win_width - 412, 370, 80, 80, 175, 370, 1)
+monster5 = Enemy_y("enemy22.png", win_width - 80, 150, 70, 70, 170, 526, 2)
 final_sprite = GameSprite('cup.png', win_width - 129, win_height - 120, 125, 110)
 
 monsters.add(monster1)
 monsters.add(monster2)
+monsters.add(monster3)
+monsters.add(monster4)
+monsters.add(monster5)
 
 finish = False
 run = True
 while run:
     if sprite.spritecollide(packman, coins, True):
         coins_amount += 1
-        if coins_amount == 4:
+        if coins_amount == 5:
             final_sprite.reset()
 
     time.delay(5)
@@ -208,7 +200,7 @@ while run:
     if not finish:
         window.blit(back, (0,0))
 
-        text = font1.render(f'Бургерів зібрано: {coins_amount}/4', True, (0, 0, 0))
+        text = font1.render(f'Бургерів зібрано: {coins_amount}/5', True, (0, 0, 0))
         window.blit(text, (10, 10))
         
         packman.update()
@@ -226,7 +218,7 @@ while run:
 
         if sprite.spritecollide(packman, coins, True):
             coins_amount += 1
-        if coins_amount == 4:
+        if coins_amount == 5:
             final_sprite.reset()
 
         if sprite.spritecollide(packman, monsters, False):
